6bfed7087286ec8328aea2cf4d3a3b50f23a6059
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2019-11-15 12:02:28 +0100

    Added `set_all` method to dynamic mask.

--- setup.py
+++ setup.py
@@ -5,12 +5,6 @@ setup(
     version='0.91',
     url='https://github.com/vphpersson/msdsalgs',
     author='vph',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3.7',
-    ],
     packages=find_packages(),
     python_requires='>=3.7'
 )
