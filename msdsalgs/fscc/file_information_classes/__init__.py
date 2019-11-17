fbf619dfc6cbff29f14e3114a31f934a53331fc7
Viktor Persson <vph.persson@gmail.com>
2019-11-17 14:30:31 +0100

    Added `FileInformationClass` enum.

--- msdsalgs/fscc/file_information_classes/__init__.py
+++ msdsalgs/fscc/file_information_classes/__init__.py
@@ -1,2 +1,15 @@
+from enum import IntEnum
+
 from .file_directory_information import FileDirectoryInformation
 from .file_id_full_directory_information import FileIdFullDirectoryInformation
+
+
+# TODO: These should be in caps, should they not?
+# TODO: Complement.
+class FileInformationClass(IntEnum):
+    FileDirectoryInformation = 0x01
+    FileFullDirectoryInformation = 0x02
+    FileIdFullDirectoryInformation = 0x26
+    FileBothDirectoryInformation = 0x03
+    FileIdBothDirectoryInformation = 0x25
+    FileNamesInformation = 0x0C
