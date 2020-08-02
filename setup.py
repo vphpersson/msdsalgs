362928b946c7e616820eb10b9b52868f7171e558
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2020-08-02 15:24:44 +0200

    Remove ndr dependency.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pycryptodome',
-        'ndr @ https://github.com/vphpersson/ndr/tarball/master',
         'pyutils @ https://github.com/vphpersson/pyutils/tarball/master'
     ]
 )
