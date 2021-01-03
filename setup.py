eb8643065e14f67b9e11bc0eedb7676e98466e6d
Viktor Persson <vph.persson@gmail.com>
2021-01-03 02:16:09 +0100

    Remove package version.

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='msdsalgs',
-    version='0.1',
     packages=find_packages(),
     install_requires=[
         'pycryptodome',
-        'pyutils @ git+ssh://git@github.com/vphpersson/pyutils.git#egg=pyutils',
+        'pyutils @ git+ssh://git@github.com/vphpersson/pyutils.git#egg=pyutils'
     ]
 )
