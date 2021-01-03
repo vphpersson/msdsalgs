0148b9f6d85f29ddd513f1a486ec5868bb332ae0
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2021-01-03 02:06:17 +0100

    Fix dependency files.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pycryptodome',
-        'pyutils @ https://github.com/vphpersson/pyutils/tarball/master'
+        'pyutils @ git+ssh://git@github.com/vphpersson/pyutils.git#egg=pyutils',
     ]
 )
