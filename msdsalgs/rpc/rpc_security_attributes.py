1a907cdcfa997875d4b69b86947141e686fdd15f
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2021-08-01 13:40:30 +0200

    Add (incomplete) `__bytes__` for `RPCSecurityAttributes`.

--- msdsalgs/rpc/rpc_security_attributes.py
+++ msdsalgs/rpc/rpc_security_attributes.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from typing import Optional
+from struct import pack
 
 from msdsalgs.rpc.rpc_security_descriptor import RPCSecurityDescriptor
 
 
 @dataclass
 class RPCSecurityAttributes:
-    rpc_security_descriptor: RPCSecurityDescriptor
-    inherit_handle: bool
+    rpc_security_descriptor: Optional[RPCSecurityDescriptor] = None
+    inherit_handle: bool = False
 
     @classmethod
     def from_bytes(cls, data: bytes) -> RPCSecurityAttributes:
@@ -16,3 +18,22 @@ class RPCSecurityAttributes:
             rpc_security_descriptor=rpc_security_descriptor,
             inherit_handle=bool(data[len(rpc_security_descriptor)])
         )
+
+    def __bytes__(self) -> bytes:
+
+        # TODO: Not sure why `bytes(12)`; it is something I observed in traffic. I think there is an NDR Pointer
+        #   involved.
+        security_description_bytes: bytes = (
+            bytes(self.rpc_security_descriptor) if self.rpc_security_descriptor is not None
+            else bytes(12)
+        )
+
+        security_description_len: int = (
+            len(security_description_bytes) if self.rpc_security_descriptor is not None else 0
+        )
+
+        return b''.join([
+            pack('<I', security_description_len),
+            security_description_bytes,
+            pack('<I', int(self.inherit_handle))
+        ])
